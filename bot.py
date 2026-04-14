c15cf634815f8d6f8ef3c970a142516c4217514b
phprint85-art <phprint85@gmail.com>
2026-04-14 17:19:01 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+from maxapi import Bot, Dispatcher
+from maxapi.types import MessageCreated
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def handle_message(event: MessageCreated):
+    chat_id = event.message.chat.id
+
+    await bot.send_message(
+        chat_id=chat_id,
+        text="Выберите филиал:",
+        inline_keyboard=[
+            [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
+            [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
+        ]
+    )
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
