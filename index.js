68615b7d6ed0869f18b7fdf3fea321a8a7ad2131
phprint85-art <phprint85@gmail.com>
2026-04-14 23:11:52 +0700

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 const max = require("@maxhub/max-bot-api");
-const Bot = max.Bot;
-const bot = new Bot(process.env.TOKEN);
 
-
-//  сообщение → меню
-bot.on("message", async (msg) => {
-    await bot.sendMessage({
-        chatId: msg.chat.id,
-        text: "Выберите филиал:",
-        inline_keyboard: [
-            [{ text: "Дачная, 27", url: "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s" }],
-            [{ text: "Красный проспект, 85", url: "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g" }]
-        ]
-    });
-});
-
-bot.start();
-
-// порт Render
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Bot is running");
-}).listen(PORT, () => {
-    console.log("Server running on", PORT);
-});
+console.log("MODULE LOADED");
