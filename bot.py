124bd197ada1b56ce84223cb3ffdf9db704e8e45
phprint85-art <phprint85@gmail.com>
2026-04-14 18:16:53 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,50 @@
 from maxapi import Bot
 import os
 import time
+import threading
+from flask import Flask
 
 TOKEN = os.getenv("TOKEN")
-
 bot = Bot(token=TOKEN)
 
-offset = 0
+app = Flask(__name__)
 
-while True:
-    try:
-        updates = bot.get_updates(offset=offset)
+@app.route("/")
+def home():
+    return "bot is running:
 
-        for update in updates:
-            print("UPDATE:", update)
+def run_bot():
+    offset = 0
 
-            offset = update["update_id"] + 1
+    while True:
+        try:
+             updates = bot.get_updates(offset=offset)
 
-            message = update.get("message")
-            if not message:
-                continue
+            for update in updates:
+                print("UPDATE:", update)
 
-            chat_id = message["chat"]["id"]
+                offset = update["update_id"] + 1
 
-            bot.send_message(
-                chat_id=chat_id,
-                text="выберите филиал:",
-                inline_keyboard=[
-                    [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
-                    [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
-                ]
-            )
+                message = update.get("message")
+                if not message:
+                    continue
 
-    except Exception as e:
-        print("ERROR:", e)
+                chat_id = message["chat"]["id"]
 
-    time.sleep(1)
+                bot.send_message(
+                    chat_id=chat_id,
+                    text="Выберите филиал:",
+                    inline_keyboard=[
+                        [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
+                        [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
+                    ]
+                )
+
+        except Exception as e:
+            print("ERROR:", e)
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    treading.Tread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=int(os.envirion.get("PORT", 5000)))
