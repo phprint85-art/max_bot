a2c6a4d7bd3f89bfb50487f0d82dbb256d6752c7
phprint85-art <phprint85@gmail.com>
2026-04-14 17:46:06 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
-from maxapi import Bot, Dispatcher
+from maxapi import Bot
 from maxapi.types import MessageCreated
-import asyncio
 import os
+import asyncio
 
 TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
 
-@dp.event(MessageCreated)
-async def handle_message(event: MessageCreated):
-    chat_id = event.message.chat.id
+async def main():
+    async for event in bot.listen():
+        print("EVENT:", event)
 
-    await bot.send_message(
-        chat_id=chat_id,
-        text="Выберите филиал:",
-        inline_keyboard=[
-            [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
-            [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
-        ]
-    )
-# регистрируем обработчик
-dp.add_handler(handle_message)
+        if isinstance(event, MessageCreated):
+            chat_id = event.message.chat.id
+
+            await bot.send_message(
+                chat_id=chat_id,
+                text="Выберите филиал:",
+                inline_keyboard=[
+                    [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
+                    [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
+                ]
+            )
 
 if __name__ == "__main__":
-    dp.run_polling(bot)
+    asyncio.run(main())
