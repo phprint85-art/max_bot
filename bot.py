56e995b012b00ffb6d0e1292ae7482624474545d
phprint85-art <phprint85@gmail.com>
2026-04-14 18:06:42 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,38 @@
 from maxapi import Bot
-#from maxapi.types import MessageCreated
 import os
-#import asyncio
+import time
 
 TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
 
-print("bot metods:")
-print(dir(bot))
+offset = 0
 
-#@bot.on(MessageCreated)
-#async def handle_message(event: MessageCreated):
-#    chat_id = event.message.chat.id
-    
-#    await bot.send_message(
-#        chat_id=chat_id,
-#        text="Выберите филиал:",
-#        inline_keyboard=[
-#            [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
-#            [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
-#        ]
-#    )
+while True:
+    try:
+        updates = bot.get_updates(offset=offset)
 
-#if __name__ == "__main__":
-#    bot.run()
+        for update in updates:
+            print("UPDATE:", update)
+
+            offset = update["update_id"] + 1
+
+            message = update.get("message")
+            if not message:
+                continue
+
+            chat_id = message["chat"]["id"]
+
+            bot.send_message(
+                chat_id=chat_id,
+                text="выберите филиал:",
+                inline_keyboard=[
+                    [{"text": "Дачная, 27", "url": "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s"}],
+                    [{"text": "Красный проспект, 85", "url": "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g"}]
+                ]
+            )
+
+    except Exception as e:
+        print("ERROR:", e)
+
+    time.sleep(1)
