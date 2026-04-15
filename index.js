46931f2e55cdf3073cbb326d62fb98ac93b6b39a
phprint85-art <phprint85@gmail.com>
2026-04-15 10:24:45 +0700

    index.js

--- index.js
+++ index.js
@@ -3,25 +3,12 @@ const http = require("http");
 
 const bot = new max.Bot(process.env.TOKEN);
 
-// любое сообщение → меню
-bot.on("message", async (msg) => {
-    await bot.sendMessage({
-        chatId: msg.chat.id,
-        text: "Выберите филиал:",
-        inline_keyboard: [
-            [
-                { text: "Дачная, 27", url: "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s" }
-            ],
-            [
-                { text: "Красный проспект, 85", url: "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g" }
-            ]
-        ]
-    });
+bot.onAny(async (event) => {
+    console.log("EVENT:", JSON.stringify(event, null, 2));
 });
 
 bot.start();
 
-// держим Render живым
 http.createServer((req, res) => {
     res.end("OK");
 }).listen(process.env.PORT || 3000);
