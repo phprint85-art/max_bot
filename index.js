710deca5b6cc937912f519396801d0e8a21d2e11
phprint85-art <phprint85@gmail.com>
2026-04-15 10:28:28 +0700

    index.js

--- index.js
+++ index.js
@@ -3,12 +3,46 @@ const http = require("http");
 
 const bot = new max.Bot(process.env.TOKEN);
 
-bot.onAny(async (event) => {
-    console.log("EVENT:", JSON.stringify(event, null, 2));
-});
+console.log("BOT STARTED");
 
-bot.start();
+let marker = 0;
 
+async function poll() {
+    try {
+        const updates = await bot.getUpdates();
+
+        console.log("UPDATES:", updates);
+
+        if (updates && updates.length > 0) {
+            for (const upd of updates) {
+
+                const msg = upd.message;
+
+                if (msg) {
+                    const chatId = msg.chat.id;
+
+                    await bot.sendMessage({
+                        chatId: chatId,
+                        text: "Выберите филиал:",
+                        inline_keyboard: [
+                            [{ text: "Дачная, 27", url: "https://max.ru/u/f9LHodD0cOICVtjg3UhFdfLtvrcH3SUeaR4e2a7Q2o-eIPbB9KBkJBfPC2s" }],
+                            [{ text: "Красный проспект, 85", url: "https://max.ru/u/f9LHodD0cOLpulUfVSlZJfTT-SQqFejmGqTlbzYKjry5cwZ2H2Za-WQh15g" }]
+                        ]
+                    });
+                }
+            }
+        }
+
+    } catch (e) {
+        console.log("ERROR:", e);
+    }
+
+    setTimeout(poll, 3000);
+}
+
+poll();
+
+// сервер для Render
 http.createServer((req, res) => {
     res.end("OK");
 }).listen(process.env.PORT || 3000);
